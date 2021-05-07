258c38d1cac36054ff1b50e8aea5f562ca2416c5
Binay Maharjan <bnaysm@gmail.com>
2021-05-07 20:56:28 +0545

    frontend app recreated using template complex-browserext

--- frontend/src/views/Popup/App.js
+++ frontend/src/views/Popup/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -21,7 +20,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           {apiResponse}
         </p>
